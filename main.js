7b88d94bfe06744d078d8bef19eaa15afbeb97c4
Heike Miersch <heike.miersch@hotmail.de>
2019-11-20 16:31:32 +0100

    delayed main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-let appId = "4d62aff061bde8448f8f3cc87a9b0e76";
 let units = "metric";
 // what people type into searchfield
 let searchMethod = "";
@@ -10,76 +9,84 @@ let searchMethod = "";
 // so it says that every single item in the searchTerm is a number
 
 function getSearchMethod(searchTerm) {
-    // if (searchTerm.length === 5 && Number.parseInt(searchTerm) + "" === searchTerm)
-    //     searchMethod = "zip";
-    // else 
-
-    searchMethod = "q";
+  // if (searchTerm.length === 5 && Number.parseInt(searchTerm) + "" === searchTerm)
+  //     searchMethod = "zip";
+  // else
+  searchMethod = "q";
 }
 
 function searchWeather(searchTerm) {
-    getSearchMethod(searchTerm);
-    // calling the url using the variables that i set up above and waiting for data
-    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result => {
-        // converting it to json    
-        return result.json();
-    }).then(result => {
-        // calling it with the result from the server
-        init(result);
+  getSearchMethod(searchTerm);
+  // calling the url using the variables that i set up above and waiting for data
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`
+  )
+    .then(result => {
+      // converting it to json
+      return result.json();
     })
+    .then(result => {
+      // calling it with the result from the server
+      init(result);
+    });
 }
 
 function init(data) {
-    switch (data.weather[0].main) {
-        case "Clear":
-            document.body.style.backgroundImage = 'url("clear.jpg")';
-            break;
-        case "Clouds":
-            document.body.style.backgroundImage = 'url("cloudy.jpg")';
-            break;
-        case "Mist":
-            document.body.style.backgroundImage = 'url("mist.jpg")';
-            break;
-        case "Rain":
-        case "Drizzle":
-            document.body.style.backgroundImage = 'url("rain.jpg")';
-            break;
-        case "Snow":
-            document.body.style.backgroundImage = 'url("snow.jpg")';
-            break;
-        case "Thunderstorm":
-            document.body.style.backgroundImage = 'url("storm.jpg")';
-            break;
-        default:
-            // document.body.style.backgroundImage = 'url("default.jpg")';
-            break;
-    }
+  switch (data.weather[0].main) {
+    case "Clear":
+      document.body.style.backgroundImage = 'url("clear.jpg")';
+      break;
+    case "Clouds":
+      document.body.style.backgroundImage = 'url("cloudy.jpg")';
+      break;
+    case "Mist":
+      document.body.style.backgroundImage = 'url("mist.jpg")';
+      break;
+    case "Rain":
+    case "Drizzle":
+      document.body.style.backgroundImage = 'url("rain.jpg")';
+      break;
+    case "Snow":
+      document.body.style.backgroundImage = 'url("snow.jpg")';
+      break;
+    case "Thunderstorm":
+      document.body.style.backgroundImage = 'url("storm.jpg")';
+      break;
+    default:
+      // document.body.style.backgroundImage = 'url("default.jpg")';
+      break;
+  }
 
-    // so now grab elements by their id and put data in
-    let weatherDescriptionKeyword = document.getElementById("weatherDescriptionKeyword");
-    let temperatureElement = document.getElementById("temperature");
-    let humidityElement = document.getElementById("humidity");
-    let city = document.getElementById("city");
-    let weatherIcon = document.getElementById("weatherIcon");
-    let tempMinElement = document.getElementById("tempMin");
-    let tempMaxElement = document.getElementById("tempMax");
+  // so now grab elements by their id and put data in
+  let weatherDescriptionKeyword = document.getElementById(
+    "weatherDescriptionKeyword"
+  );
+  let temperatureElement = document.getElementById("temperature");
+  let humidityElement = document.getElementById("humidity");
+  let city = document.getElementById("city");
+  let weatherIcon = document.getElementById("weatherIcon");
+  let tempMinElement = document.getElementById("tempMin");
+  let tempMaxElement = document.getElementById("tempMax");
 
-    weatherIcon.src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
-    resultDescription = data.weather[0].description;
-    weatherDescriptionKeyword.innerText = resultDescription.toUpperCase();
-    temperatureElement.innerHTML = "temperature " + (Math.round(data.main.temp * 10) / 10) + " °C";
-    city.innerHTML = data.name + ", " + data.sys.country;
-    humidityElement.innerHTML = "humidity: " + data.main.humidity + " %";
-    tempMinElement.innerHTML = "min temperature: " + (Math.round(data.main.temp_min * 10) / 10) + " %";
-    tempMaxElement.innerHTML = "max temperature: " + (Math.round(data.main.temp_max * 10) / 10) + " %";
+  weatherIcon.src =
+    "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+  resultDescription = data.weather[0].description;
+  weatherDescriptionKeyword.innerText = resultDescription.toUpperCase();
+  temperatureElement.innerHTML =
+    "temperature " + Math.round(data.main.temp * 10) / 10 + " °C";
+  city.innerHTML = data.name + ", " + data.sys.country;
+  humidityElement.innerHTML = "humidity: " + data.main.humidity + " %";
+  tempMinElement.innerHTML =
+    "min temperature: " + Math.round(data.main.temp_min * 10) / 10 + " %";
+  tempMaxElement.innerHTML =
+    "max temperature: " + Math.round(data.main.temp_max * 10) / 10 + " %";
 
-    console.log(data);
+  // console.log(data);
 }
 
 // hook up the searchbutton
 document.getElementById("searchButton").addEventListener("click", () => {
-    let searchTerm = document.getElementById("searchInput").value;
-    // so if searchTerm exists (if), please call function
-    if (searchTerm)
-        searchWeather(searchTerm);
-})
+  let searchTerm = document.getElementById("searchInput").value;
+  // so if searchTerm exists (if), please call function
+  if (searchTerm) searchWeather(searchTerm);
+});
